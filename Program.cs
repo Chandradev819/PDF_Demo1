09b41f4eae2d2ae78f252ff5f3001ed3886e44b3
Chandradev Prasad <chandradev.prasad@channelmechanics.com>
2023-01-22 17:43:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using PDF_Demo1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".res"] = "application/octet-stream";
+provider.Mappings[".pexe"] = "application/x-pnacl";
+provider.Mappings[".nmf"] = "application/octet-stream";
+provider.Mappings[".mem"] = "application/octet-stream";
+provider.Mappings[".wasm"] = "application/wasm";
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+    ContentTypeProvider = provider
+});
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
